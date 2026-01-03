f054b89337ebfab9beff02ccf6509c6a0019d340
Samuel Resende <samuelres444@gmail.com>
2026-01-03 13:58:12 -0300

    Implementa a classe Device e seus respectivos atributos

--- login_system/src/main/java/com/samuel/login_system/repository/model/Device.java
+++ login_system/src/main/java/com/samuel/login_system/repository/model/Device.java
@@ -1,5 +1,34 @@
 package com.samuel.login_system.repository.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_devices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Device {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name; // Nome do dispositivo "Iphone 11 pro max"
+
+    @Column(nullable = false)
+    private String status; // "On" ou "OFF"
+
+
+    // Relacao com user 
+    @ManyToMany
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
 }
